6eacc3d47ab170d4b8353a6e3e7b1bbefc9f6b7f
RandomDevGuy <84237344+tpfirman@users.noreply.github.com>
2025-03-08 22:25:14 +0800

    implement Salesforce authentication and query user data

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
+from simple_salesforce import Salesforce
+from libs.sfPyAuth.src.sfPyAuth.sfPyAuth import oAuthController
+
+
+
+sfAuth = oAuthController()
+sfInstance = Salesforce(instance_url=sfAuth.sf_instanceUrl, session_id=sfAuth.sm.accessToken)
+    
+def __init__(self):
+    if sfAuth.sm.accessToken == None:
+        print("No Access Token. Auth is broken. Exiting.")
+        exit(1)
+
 def main():
-    print("Hello, World!")
+    result = sfInstance.query("SELECT Id FROM User LIMIT 5")
+    print(result)
 
 if __name__ == "__main__":
     main()
